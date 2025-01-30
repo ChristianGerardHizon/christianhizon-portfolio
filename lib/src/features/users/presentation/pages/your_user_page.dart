610683ca406f5eefb19836553de2de2f1063518b
Christian Hizon <christiangerardhizon@gmail.com>
2025-01-30 22:03:00 +0800

    updates

--- lib/src/features/users/presentation/pages/your_user_page.dart
+++ lib/src/features/users/presentation/pages/your_user_page.dart
@@ -1,20 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:gym_system/src/core/routing/main.routes.dart';
+import 'package:gym_system/src/core/widgets/app_snackbar.dart';
+import 'package:gym_system/src/features/authentication/presentation/controllers/auth_controller.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class YourUserPage extends HookConsumerWidget {
   const YourUserPage({super.key});
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    ///
+    ///
+    ///
+    onLogout() async {
+      final result =
+          await ref.read(authControllerProvider.notifier).logout().run();
+      result.fold(
+        (l) => AppSnackBar.rootError(message: l.toString()),
+        (r) => const LoginPageRoute().go(context),
+      );
+    }
+
     return Scaffold(
       body: CustomScrollView(
         slivers: [
           SliverAppBar(
             title: Text('App Bar Sample'),
           ),
-          SliverAppBar(
-            pinned: true,
-            title: Text('Contetn'),
-          )
+          SliverList.list(children: [
+            ListTile(
+              leading: Icon(
+                Icons.logout,
+                color: Theme.of(context).colorScheme.error,
+              ),
+              title: Text(
+                'Logout',
+                style: TextStyle(color: Theme.of(context).colorScheme.error),
+              ),
+              onTap: onLogout,
+            ),
+          ])
         ],
       ),
     );
