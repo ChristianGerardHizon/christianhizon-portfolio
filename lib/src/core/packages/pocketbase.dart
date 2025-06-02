93051fea02ba5c3cc2708e36a56c4af6f075e837
Christian Hizon <christiangerardhizon@gmail.com>
2025-06-02 15:31:25 +0800

    updates

--- lib/src/core/packages/pocketbase.dart
+++ lib/src/core/packages/pocketbase.dart
@@ -6,12 +6,12 @@ part 'pocketbase.g.dart';
 
 @Riverpod(keepAlive: true)
 PocketBase pocketbase(Ref ref) {
-  return PocketBase('https://hizonelaundry.sannjosevet.xyz/');
+  // return PocketBase('https://hizonelaundry.sannjosevet.xyz/');
 
   // if (kDebugMode) {
   //   return PocketBase('https://dev.sannjosevet.xyz');
   //   // return PocketBase('http://127.0.0.1:8090');
   // } else {
-  //   return PocketBase('https://www.sannjosevet.xyz');
+    return PocketBase('https://www.sannjosevet.xyz');
   // }
 }
