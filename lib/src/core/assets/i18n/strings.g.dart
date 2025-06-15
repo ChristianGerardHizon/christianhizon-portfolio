bfefeeaf4abdd8630021a93102aa49c79c1892b1
Christian Hizon <christiangerardhizon@gmail.com>
2025-06-16 03:09:00 +0800

    updates

--- lib/src/core/assets/i18n/strings.g.dart
+++ lib/src/core/assets/i18n/strings.g.dart
@@ -6,7 +6,7 @@
 /// Locales: 2
 /// Strings: 24 (12 per locale)
 ///
-/// Built on 2025-06-02 at 07:28 UTC
+/// Built on 2025-06-09 at 18:05 UTC
 
 // coverage:ignore-file
 // ignore_for_file: type=lint, unused_import
